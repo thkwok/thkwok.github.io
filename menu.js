b7f519db416268952c462ca789c924044f051c54
Tsz Ho Kwok <tszho.kwok@concordia.ca>
2024-08-18 16:53:29 -0400

    update

---
+++ menu.js
@@ -0,0 +1,15 @@
+document.write('\
+<div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">\
+	<ul id="nav" class="navbar-nav ms-auto">\
+		<li class="nav-item">\
+			<a id="index" href="index.html" aria-label="Toggle navigation">Home</a>\
+		</li>\
+		<li class="nav-item">\
+			<a id="publication" href="publication.html" aria-label="Toggle navigation">Publication</a>\
+		</li>\
+	</ul>\
+</div>\
+<div class="button">\
+	<a href="https://www.jotform.com/tszhokwok/GS_Apply" class="signup" target=new>Want to Join Us?</a>\
+</div>\
+');
