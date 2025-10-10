d98f0f3344c6283f990f16d5ffb0e1dfca2c3709
Tsz Ho Kwok <43834657+thkwok@users.noreply.github.com>
2025-10-10 10:08:41 -0400

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,17 @@
+document.write('\
+    <footer class="footer section">\
+        <div class="footer-top">\
+            <div class="container">\
+                <div class="inner-content">\
+                    <div class="row">\
+						<hr>\
+						<p>Chase excellence, success will follow. - 3 Idiots<br/>\
+							If you really want to do something, you\'ll find a way. If you don\'t, you\'ll find an excuse. - Jim Rohn<br/>\
+							The simplicity of true greatness, the open mind of true wisdom, and the meekness of true strength. - MacArthur<br/>\
+							It is a good day if you have learned something new.</p>\
+                    </div>\
+                </div>\
+            </div>\
+        </div>\
+    </footer>\
+');
